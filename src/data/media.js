499b14db9d5dd019f4a5f703ab0f928547d83517
David Andersson <lejonmanen@gmail.com>
2026-04-01 12:05:20 +0200

    Home page done

---
+++ src/data/media.js
@@ -0,0 +1,15 @@
+
+// Gratis rymdbilder från NASA
+const media = [
+	{ id: 1, title: 'Earth', src: 'earth.jpg', url: 'https://images.nasa.gov/details/PIA18033' },
+	{ id: 2, title: 'James Webb Space Telescope NIRCam Image of the "Cosmic Cliffs" in Carina Nebula', src: 'carina-nabula--james-webb.jpg', url: 'https://images.nasa.gov/details/carina_nebula' },
+	{ id: 3, title: 'Andromeda Galaxy', src: 'andromeda-galaxy.jpg', url: 'https://images.nasa.gov/details/PIA04921' },
+	{ id: 4, title: 'The Earth & Moon', src: 'earth-and-moon.jpg', url: 'https://images.nasa.gov/details/PIA00342' },
+	{ id: 5, title: 'Astronaut Edwin Aldrin walks on lunar surface near leg of Lunar Module', src: 'lunar-walk.jpg', url: 'https://images.nasa.gov/details/as11-40-5903' },
+	{ id: 6, title: 'Hubble Sees a Star "Inflating" a Giant Bubble', src: 'hubble-star-inflating-bubble.jpg', url: 'https://images.nasa.gov/details/GSFC_20171208_Archive_e000383' },
+	{ id: 7, title: "Hubble Captures Vivid Auroras in Jupiter's Atmosphere", src: 'jupiter-aurora.jpg', url: 'https://images.nasa.gov/details/hubble-captures-vivid-auroras-in-jupiters-atmosphere_28000029525_o' },
+	{ id: 8, title: 'The Moon', src: 'the-moon.jpg', url: 'https://images.nasa.gov/details/GSFC_20171208_Archive_e001982' },
+	{ id: 9, title: 'A Sky View of Earth From Suomi NPP', src: 'sky-view-earth.jpg', url: 'https://images.nasa.gov/details/a-sky-view-of-earth-from-suomi-npp_16611703184_o' },
+]
+
+export { media }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
+// Använd "import" när bilden ligger under /src
+import earthImg from './earth.jpg'
+import './Home.css'
+
 const Home = () => {
 
 	return (
-		<>
-			home
-		</>
+		<div className="home-page">
+			<h2> Welcome to space! </h2>
+			<p> The images are from NASA. </p>
+			<img src={earthImg} alt="Earth" />
+		</div>
 	)
 }
 
