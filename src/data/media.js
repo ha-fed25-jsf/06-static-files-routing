aab36cf94b79ad978bd45d4914658d7a7f299df0
David Andersson <lejonmanen@gmail.com>
2026-04-01 12:22:23 +0200

    Gallery done

--- src/data/media.js
+++ src/data/media.js
@@ -4,7 +4,7 @@ const media = [
 	{ id: 1, title: 'Earth', src: 'earth.jpg', url: 'https://images.nasa.gov/details/PIA18033' },
 	{ id: 2, title: 'James Webb Space Telescope NIRCam Image of the "Cosmic Cliffs" in Carina Nebula', src: 'carina-nabula--james-webb.jpg', url: 'https://images.nasa.gov/details/carina_nebula' },
 	{ id: 3, title: 'Andromeda Galaxy', src: 'andromeda-galaxy.jpg', url: 'https://images.nasa.gov/details/PIA04921' },
-	{ id: 4, title: 'The Earth & Moon', src: 'earth-and-moon.jpg', url: 'https://images.nasa.gov/details/PIA00342' },
+	// { id: 4, title: 'The Earth & Moon', src: 'earth-and-moon.jpg', url: 'https://images.nasa.gov/details/PIA00342' },
 	{ id: 5, title: 'Astronaut Edwin Aldrin walks on lunar surface near leg of Lunar Module', src: 'lunar-walk.jpg', url: 'https://images.nasa.gov/details/as11-40-5903' },
 	{ id: 6, title: 'Hubble Sees a Star "Inflating" a Giant Bubble', src: 'hubble-star-inflating-bubble.jpg', url: 'https://images.nasa.gov/details/GSFC_20171208_Archive_e000383' },
 	{ id: 7, title: "Hubble Captures Vivid Auroras in Jupiter's Atmosphere", src: 'jupiter-aurora.jpg', url: 'https://images.nasa.gov/details/hubble-captures-vivid-auroras-in-jupiters-atmosphere_28000029525_o' },
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,9 +1,19 @@
+import './Gallery.css'
+import { media } from '../data/media.js'
+// Eftersom filerna mappas dynamiskt, kan vi inte använda import. Filerna måste ligga under /public och länkas med direktlänk. Vi lägger till /media eftersom filerna kommer hamna i /dist/media/
+
 const Gallery = () => {
 
 	return (
-		<>
-			gallery
-		</>
+		<div className="gallery-page">
+			{media.map(img => (
+				<section key={img.id} className="img-card">
+					<img src={'/media/' + img.src} alt={img.title} />
+					<h3> {img.title} </h3>
+					<a href={img.url}> More information... </a>
+				</section>
+			))}
+		</div>
 	)
 }
 
